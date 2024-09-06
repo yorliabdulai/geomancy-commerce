d0b24979c5333affb79d1f02e8d26f53438fe47e
yorliabdulai <yorliabdulai1@gmail.com>
2024-09-06 16:33:59 +0000

    chore: Update server.js to allow CORS from localhost:5173 and specify allowed methods and headers

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:5173', // Allow only your frontend
-    methods: 'GET,POST,PUT,DELETE',  // Specify the allowed methods
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',  // Specify the allowed methods
     allowedHeaders: 'Content-Type,Authorization' // Specify allowed headers
 }));
 
